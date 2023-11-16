884378223f2295275b8bd84a934cd5fa0ff10249
YuJung Park <87461688+elenaisnanocat@users.noreply.github.com>
2023-11-16 13:46:29 +0900

    Title: 특정 규칙에 따른 숫자 출력, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231116/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int cnt = 1;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(j >= i) {
+                if(cnt == 10) {
+                    cnt = 1;
+                }
+                cout << cnt << " ";
+                cnt++;
+            }
+            else {
+                cout << "  ";
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
