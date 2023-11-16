e203db5583774523e45643bdd6cac0cb8c57d1bc
YuJung Park <87461688+elenaisnanocat@users.noreply.github.com>
2023-11-16 16:26:47 +0900

    Title: 알파벳 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231116/알파벳 출력하기/print-alphabet.cpp
+++ 231116/알파벳 출력하기/print-alphabet.cpp
@@ -10,8 +10,8 @@ int main() {
     for(int i = 0; i < n; i++) {
         for(int j = 0; j <= i; j++) {
             cout << c;
-            if(c == 'Z') c = 'A';
             c++;
+            if(c == 'Z') c = 'A';
         }
         cout << "\n";
     }
