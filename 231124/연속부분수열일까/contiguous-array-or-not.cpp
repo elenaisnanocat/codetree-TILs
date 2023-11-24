eb1f87fc9b3b10606753105e159eea896a31b945
YuJung Park <87461688+elenaisnanocat@users.noreply.github.com>
2023-11-24 09:03:26 +0900

    Title: 연속부분수열일까, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231124/연속부분수열일까/contiguous-array-or-not.cpp
+++ 231124/연속부분수열일까/contiguous-array-or-not.cpp
@@ -6,7 +6,7 @@ int main() {
     int n1, n2;
     cin >> n1 >> n2;
 
-    int arr1[100] = {}, arr2[100] = {};
+    int arr1[100], arr2[100];
 
     for(int i = 0; i < n1; i++) cin >> arr1[i];
     for(int i = 0; i < n2; i++) cin >> arr2[i];
