c04ce3f7671c3edd43d1593cbd276536fd38a4c7
YuJung Park <87461688+elenaisnanocat@users.noreply.github.com>
2023-11-27 13:25:48 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231127/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, arr[1001] = {};
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+    int buy = arr[0], idx = 0;
+
+    for(int i = 0; i < n; i++) {
+        if(buy > arr[i]) {
+            buy = arr[i];
+            idx = i;
+        }
+    }
+    //cout << buy;
+    
+    //판매
+    int max_price = arr[idx];
+    
+    if((idx + 1) == n) {
+        cout << 0;
+    }
+    else{
+        for(int sale = idx; sale < n; sale++) {
+            if(arr[sale] > max_price) {
+                max_price = arr[sale];
+            }
+        }
+    }
+    cout << max_price - buy;
+    return 0;
+}
