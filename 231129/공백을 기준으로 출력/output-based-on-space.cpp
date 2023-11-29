02ce974d1e2db4946962060f17e731beeff87d4d
YuJung Park <87461688+elenaisnanocat@users.noreply.github.com>
2023-11-29 12:44:17 +0900

    Title: 공백을 기준으로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231129/공백을 기준으로 출력/output-based-on-space.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str1, str2;
+
+    getline(cin, str1);
+    for(int i = 0; i < str1.length(); i++){
+        if(str1[i] != ' ') cout << str1[i];
+    }
+    getline(cin, str2);
+    for(int i = 0; i < str2.length(); i++){
+        if(str2[i] != ' ') cout << str2[i];
+    }
+    return 0;
+}
