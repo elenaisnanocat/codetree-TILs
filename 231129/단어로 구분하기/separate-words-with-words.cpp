ccf3611ff33ad32f1e25d551c4f84a095558c190
YuJung Park <87461688+elenaisnanocat@users.noreply.github.com>
2023-11-29 13:59:32 +0900

    Title: 단어로 구분하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231129/단어로 구분하기/separate-words-with-words.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    
+    string str[10];
+
+    for(int i = 0; i < 10; i++) {
+        cin >> str[i];
+        cout << str[i] << "\n";
+    }
+    return 0;
+}
