625c5d2ff71f54a9e33d450e09424fb100343e82
YuJung Park <87461688+elenaisnanocat@users.noreply.github.com>
2023-12-06 16:30:49 +0900

    Title: 문자열 밀기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231206/문자열 밀기/push-char.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int len = s.size();
+    s = s.substr(1, len - 1) + s.substr(0, 1);
+    cout << s;
+    return 0;
+}
