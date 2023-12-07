8fe2cf2c46a78f4ad10c748ec5cb302ae0259e4f
YuJung Park <87461688+elenaisnanocat@users.noreply.github.com>
2023-12-07 13:27:03 +0900

    Title: 그 다음 알파벳, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231207/그 다음 알파벳/next-alphabet.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+//'a' 97
+int main() {
+    char c;
+    cin >> c;
+    
+    char ans;
+    if(c != 'z') {
+        ans = (int)c + 1;
+    }
+    else ans = (int)c - (int)'a';
+
+    cout << ans;
+    return 0;
+}
