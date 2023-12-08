15ea4a89c803022089254b43e0507cf1eb8ee29b
YuJung Park <87461688+elenaisnanocat@users.noreply.github.com>
2023-12-08 10:06:25 +0900

    Title: 각 자리 숫자들의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231208/각 자리 숫자들의 합/sum-of-each-digit.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    string s = to_string(n);
+
+    int sum = 0;
+    for(int i = 0; i < s.size(); i++) {
+        sum += s[i] - '0';
+    }
+    cout << sum;
+    return 0;
+}
