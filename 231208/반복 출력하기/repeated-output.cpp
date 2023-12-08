53cf734e0279badb87d690382e3fd560be131acb
YuJung Park <87461688+elenaisnanocat@users.noreply.github.com>
2023-12-08 16:12:00 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231208/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void Print(int n) {
+    for(int i = 0; i < n; i++) {
+        cout << "12345^&*()_" << "\n";
+    }
+    
+}
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    Print(n);
+    return 0;
+}
