f16677846944271a41cd2b4f2d46fcd3ca861953
YuJung Park <87461688+elenaisnanocat@users.noreply.github.com>
2023-12-18 18:29:09 +0900

    Title: 함수를 이용한 윤년 판별, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 231218/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
+++ 231218/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
@@ -3,14 +3,13 @@
 using namespace std;
 
 bool yun(int n) {
-    if(n % 4 == 0)
-        return true;
-    if(n % 4 == 0 && n % 100 == 0)
+    if(n % 4 != 0)
         return false;
-    if(n % 4 == 0 && n % 100 == 0 && n % 400 == 0)
+    if(n % 100 != 0)
         return true;
-    else
-        return false;
+    if(n % 400 == 0)
+        return true;    
+    return false;
 }
 
 int main() {
