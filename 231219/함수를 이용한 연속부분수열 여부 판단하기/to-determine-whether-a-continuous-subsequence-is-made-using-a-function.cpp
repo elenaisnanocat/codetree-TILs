a36dc2934439aca4d8a70dd094d30f173689184f
YuJung Park <87461688+elenaisnanocat@users.noreply.github.com>
2023-12-19 19:51:18 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231219/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+
+int arra[100], arrb[100];
+
+bool IsSame(int n) {
+    for(int i = 0; i < m; i++) {
+        if(arra[i + n] != arrb[i])
+            return false;
+    } return true;
+}
+
+bool IsSubsequence() {
+    for(int i = 0; i <= n - m; i++)
+        if(IsSame(i))
+            return true;
+    return false;
+}
+
+
+int main() {
+
+    for(int i = 0; i <= n; i++){
+        cin >> arra[i];
+    }
+    for(int j = 0; j <= m; j++) {
+        cin >> arrb[j];
+    }
+
+    if(IsSubsequence()) cout << "Yes";
+    else cout << "No";
+
+    return 0;
+}
