5a32ea414b62459f617b4afdadbf6bc18d061dd0
YuJung Park <87461688+elenaisnanocat@users.noreply.github.com>
2024-01-08 14:48:10 +0900

    Title: DateTime to DateTime, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240108/DateTime to DateTime/datetime-to-datetime.cpp
+++ 240108/DateTime to DateTime/datetime-to-datetime.cpp
@@ -9,7 +9,7 @@ int main() {
     if(a == 11 && b == 11 && c < 10) {
         cout << -1;
     }
-    else if(b < 11) cout << -1;
+    else if(a == 11 && b < 11) cout << -1;
     else if(a == 11 && b == 11 && c == 11) cout << 0;
     else cout << (a - 11) * 24 * 60 + (b - 11) * 60 + c - 11;
 
