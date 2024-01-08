5984b30fd7f2da9e4d33b1a075575981eb5aa5c2
YuJung Park <87461688+elenaisnanocat@users.noreply.github.com>
2024-01-08 14:46:48 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240108/DateTime to DateTime/datetime-to-datetime.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if(b == 11 && c < 10) {
+        cout << -1;
+    }
+    else if(b < 11) cout << -1;
+    else if(a == 11 && b == 11 && c == 11) cout << 0;
+    else cout << (a - 11) * 24 * 60 + (b - 11) * 60 + c - 11;
+
+    return 0;
+}
