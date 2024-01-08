de6edd932e8d3fef7642a7211e571e9103fd8ea2
YuJung Park <87461688+elenaisnanocat@users.noreply.github.com>
2024-01-08 14:19:39 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240108/Date to Date/date-to-date.cpp
+++ 240108/Date to Date/date-to-date.cpp
@@ -8,31 +8,18 @@ int num_of_days[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 int main() {
     cin >> m1 >> d1 >> m2 >> d2;
-    //m1
+    
     int later = 0, first = 0;
-    if(m1 >= m2) {
-        for(int i = 1; i <= m1; i++) {
-            later += num_of_days[i];
-        }
-        later += d1;
-        for(int i = 1; i <= m2; i++) {
-            first += num_of_days[i];
-        }
-        first += d2;
+    for(int i = 1; i < m1; i++) {
+        later += num_of_days[i];
     }
-    else {
-        for(int i = 1; i <= m1; i++) {
-            first += num_of_days[i];
-        }
-        first += d1;
-        for(int i = 1; i <= m2; i++) {
-            later += num_of_days[i];
-        }
-        later += d2;
+    later += d1;
+    for(int i = 1; i < m2; i++) {
+        first += num_of_days[i];
     }
+    first += d2;
+
     
-    if(first == later) cout << 1;
-    else cout << later - first;
-    
+    cout << first - later + 1;
     return 0;
 }
