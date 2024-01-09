924b3c60cd8957beb27b1f1b2c609485110df3bc
YuJung Park <87461688+elenaisnanocat@users.noreply.github.com>
2024-01-09 17:15:39 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240109/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+#define MAX_N 100
+#define MAX_R 2000
+#define OFFSET 1000
+
+using namespace std;
+
+int n;
+int x1[MAX_N], x2[MAX_N], checked[MAX_R + 1];
+
+int main() {
+    cin >> n;
+    
+    int cur = 0;
+    for(int i = 0; i < n; i++) {
+        int dis;
+        char dir;
+        cin >> dis >> dir;
+
+        if(dir == 'L') {
+            x1[i] = cur - dis;
+            x2[i] = cur;
+            cur -= dis;
+        }
+        else {
+            x1[i] = cur;
+            x2[i] = cur + dis;
+            cur += dis;
+        }
+
+        x1[i] += OFFSET;
+        x2[i] += OFFSET;
+    }
+
+    for(int i = 0; i < n; i++)
+        for(int j = x1[i]; j < x2[i]; j++) checked[j]++;
+    
+    int cnt = 0;
+    for(int i = 0; i <= MAX_R; i++)
+        if(checked[i] >= 2) cnt++;
+    
+    cout << cnt;
+    
+    return 0;
+}
