ee775db01f2f14267b78c3fc18e0c18478582642
YuJung Park <87461688+elenaisnanocat@users.noreply.github.com>
2024-06-21 14:39:26 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 1037ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240621/빙빙 돌며 사각형 채우기/snail-alphabet-square.cpp
+++ 240621/빙빙 돌며 사각형 채우기/snail-alphabet-square.cpp
@@ -7,7 +7,8 @@ using namespace std;
 
 int n, m, r, c, dir;
 
-char arr[MAX_N][MAX_N];
+char arr[MAX_N][MAX_N]= {0};
+bool visit[MAX_N][MAX_N] = {0};
 
 int dr[DIR_NUM] = {0, 1, 0, -1};
 int dc[DIR_NUM] = {1, 0, -1, 0};
@@ -19,15 +20,17 @@ bool InRange(int r, int c) {
 int main() {
     cin >> n >> m;
     arr[0][0] = 'A';
+    visit[0][0] = true;
 
     for(int num = 1; num < n * m; num++) {
         while(true) {
             int nr = r + dr[dir];
             int nc = c + dc[dir];
 
-            if(InRange(nr, nc) && arr[nr][nc] == 0) {
+            if(InRange(nr, nc) && visit[nr][nc] == false) {
                 r = nr;
                 c = nc;
+                visit[r][c] = true;
                 arr[r][c] =+ (char)(num % 26 + 'A');
                 break;
             }
