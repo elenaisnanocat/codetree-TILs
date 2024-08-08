f742ab9776ce26fbf00fd3d6de128e1b9e5652c7
YuJung Park <87461688+elenaisnanocat@users.noreply.github.com>
2024-08-08 09:31:50 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 240808/괄호 쌍 만들어주기 2/pair-parentheses-2.cpp
+++ 240808/괄호 쌍 만들어주기 2/pair-parentheses-2.cpp
@@ -13,7 +13,7 @@ int main() {
 
     int cnt = 0;
     for(int i = 0; i < n - 1; i++) {
-        for(int j = 0; j < n - 1; j++) {
+        for(int j = i + 1; j < n - 1; j++) {
             if(str[i] == '(' && str[i + 1] == '(' && str[j] == ')' && str[j + 1] == ')') cnt++;
 
         }
