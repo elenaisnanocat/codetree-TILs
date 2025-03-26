fb84853579a089fc766a0c383303698e5ff1cf79
elenaisnanocat <87461688+elenaisnanocat@users.noreply.github.com>
2025-03-26 22:19:57 +0900

    Title: 친근하지 않은 수, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250326/친근하지 않은 수/unfriendly-number.cpp
+++ 250326/친근하지 않은 수/unfriendly-number.cpp
@@ -6,7 +6,7 @@ int main() {
     cin >> n;
 
     int cnt = 0; 
-    for(int i = 1; i < n; i++) {
+    for(int i = 1; i <= n; i++) {
         if(i % 2 == 0 || i % 3 == 0 || i % 5 ==0) continue;
         else cnt++;
     }
