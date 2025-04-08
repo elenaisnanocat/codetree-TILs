5737c7184f4d00e56861786b2acb352fcee6323e
elenaisnanocat <87461688+elenaisnanocat@users.noreply.github.com>
2025-04-08 19:21:33 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250408/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Total days in Year" << "\n" << 365 << "\n" << "Circumference rate" << "\n";
+    cout.precision(10);
+
+    cout << fixed;
+    cout << 3.1415926535;
+    return 0;
+}
