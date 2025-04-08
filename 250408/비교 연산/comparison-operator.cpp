b9dad0f47f0300934c1141efdb7af0c89e17b5c8
elenaisnanocat <87461688+elenaisnanocat@users.noreply.github.com>
2025-04-08 18:26:09 +0900

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250408/비교 연산/comparison-operator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+	// 입력
+	cin >> a >> b;
+    
+    // 출력
+    cout << (a >= b) << endl;
+	cout << (a > b) << endl;
+	cout << (a <= b) << endl;
+	cout << (a < b) << endl;
+	cout << (a == b) << endl;
+	cout << (a != b) << endl;
+    return 0;
+}
