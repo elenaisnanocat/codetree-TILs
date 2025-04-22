18abde21ddea133f71a2814e943ea437461cc92a
elenaisnanocat <87461688+elenaisnanocat@users.noreply.github.com>
2025-04-22 15:37:54 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250422/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int f, s, e;
+    cin >> f;
+    cin.get();
+    cin >> s;
+    cin.get();
+    cin >> e;
+
+    cout << "010-" << e << "-" << s;
+    return 0;
+}
