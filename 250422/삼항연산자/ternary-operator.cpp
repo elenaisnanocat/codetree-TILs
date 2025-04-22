f2ce1429fcf565d17c15f598739a804f533e703d
elenaisnanocat <87461688+elenaisnanocat@users.noreply.github.com>
2025-04-22 18:47:15 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250422/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int score;
+    cin >> score;
+
+    string ans = score == 100 ? "pass" : "failure";
+    cout << ans;
+    return 0;
+}
