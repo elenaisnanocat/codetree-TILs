30194fa34dc0b86551ba2c07beef95281af4d888
elenaisnanocat <87461688+elenaisnanocat@users.noreply.github.com>
2025-04-22 15:33:42 +0900

    Title: 입력받아 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250422/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int fir, ed;
+    cin >> fir;
+    cin.get();
+    cin >> ed;
+    cin.get();
+
+    cout << fir << ed;
+    return 0;
+}
